8e6b2a7cbec5a7a61e3989909c26181299d0d47b
lukebrown14-code <lukebrown14@proton.me>
2025-05-30 09:22:21 +0800

    Remove comment out code

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,37 +1,3 @@
-// import type { PageLoad } from './$types';
-// import type { PostsPageData } from '$lib/types';
-// import { error } from '@sveltejs/kit';
-
-// export const load = (async () => {
-// 	try {
-// 		return {
-// 			content: {
-// 				1: {
-// 					id: 1,
-// 					title: 'A-Brief-History-of-England',
-// 					subTitle: 'From Stonehenge to Empire: A Journey Through Time',
-// 					tags: ['England', 'History']
-// 				},
-// 				2: {
-// 					id: 2,
-// 					title: 'Post',
-// 					subTitle: 'Post 2',
-// 					tags: ['four', 'three']
-// 				},
-// 				3: {
-// 					id: 3,
-// 					title: 'Post-two',
-// 					subTitle: 'The trust about 1',
-// 					tags: ['four', 'three']
-// 				}
-// 			}
-// 		};
-// 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// 	} catch (e) {
-// 		throw error(500, 'Posts not found');
-// 	}
-// }) satisfies PageLoad<PostsPageData>;
-
 import { supabase } from '$lib/superbaseClient';
 
 export async function load() {
