1ae824ef7788036c11858e5c7998ec6e9851bc70
lukebrown14-code <lukebrown14@proton.me>
2025-05-30 09:41:05 +0800

    Added .env file

--- src/lib/superbaseClient.js
+++ src/lib/superbaseClient.js
@@ -1,6 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/private';
 
-export const supabase = createClient(
-	'https://imcsotitcoqjopyeteiq.supabase.co',
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltY3NvdGl0Y29xam9weWV0ZWlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NjQ3MjcsImV4cCI6MjA2NDE0MDcyN30.tkpdtNoCHKZ6qGn7qUlBipR6xsuT6i6HFooE66-qXLo'
-);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
