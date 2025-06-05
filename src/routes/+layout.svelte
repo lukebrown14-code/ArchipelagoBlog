30f704d76869775cf2a435802a2bca04b3034686
lukebrown14-code <lukebrown14@proton.me>
2025-06-06 01:06:10 +0800

    Updated styling of article section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,10 @@
 	let { children } = $props();
 </script>
 
-<div class="mx-6">
-	<div class="justify-left relative top-4 flex">
-		<h1
-			id="title"
-			class="link link-transition bg-red-400 text-6xl font-bold decoration-4 underline-offset-4"
-		>
-			<a href="/">AR·CHI·PEL·AGO</a>
+<div class="container mx-auto">
+	<div class="relative top-4 flex justify-center">
+		<h1 id="title" class="link text-6xl font-bold decoration-4 underline-offset-4">
+			<a href="/" data-sveltekit-preload-data>AR·CHI·PEL·AGO</a>
 		</h1>
 	</div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,18 @@
 	import '../app.css';
 </script>
 
-<div>
-	<h1 class="title text-left text-5xl">AR·TI·CLES</h1>
-	<div class="mt-4 pl-4">
+<div class="px-4">
+	<h1 class="mb-6 text-5xl font-bold underline decoration-4 underline-offset-4">AR·TI·CLES</h1>
+
+	<div class="space-y-4">
 		{#each data.BlogData as post (post.id)}
-			<div class="mt-2">
-				<h2 class="link-transition link right-0 text-3xl decoration-2 underline-offset-2">
-					<a href="/posts/{post.title}">{post.title.replaceAll('-', ' ')}</a>
+			<div>
+				<h2 class="link mx-4 text-4xl font-semibold decoration-2 underline-offset-2">
+					<a href="/posts/{post.title}">
+						{post.title.replaceAll('-', ' ')}
+					</a>
 				</h2>
-				<p class="mx-2 text-2xl opacity-80">{post.subTitle}</p>
+				<p class="mx-8 mt-2 text-3xl opacity-80">{post.subTitle}</p>
 			</div>
 		{/each}
 	</div>
