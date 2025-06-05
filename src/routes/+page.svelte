f4df0dedd68b4d6e245c3fd60145fe0efd3bd210
lukebrown14-code <lukebrown14@proton.me>
2025-06-06 00:17:34 +0800

    Update homepage style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,14 +4,14 @@
 </script>
 
 <div>
-	<h1 class="title text-left text-5xl">READ·ING</h1>
-	<div class="mt-4">
+	<h1 class="title text-left text-5xl">AR·TI·CLES</h1>
+	<div class="mt-4 pl-4">
 		{#each data.BlogData as post (post.id)}
 			<div class="mt-2">
-				<h2 class="title-hover title text-xl">
+				<h2 class="link-transition link right-0 text-3xl decoration-2 underline-offset-2">
 					<a href="/posts/{post.title}">{post.title.replaceAll('-', ' ')}</a>
 				</h2>
-				<p class="subTitle mx-2 text-sm">{post.subTitle} - {post.tags.join(', ')}</p>
+				<p class="mx-2 text-2xl opacity-80">{post.subTitle}</p>
 			</div>
 		{/each}
 	</div>
